315f531ca76cad98becea99582762f836b99c7f9
Tomas Doran <bobtfish@bobtfish.net>
2009-12-09 01:37:27 +0000

    Import of BOBTFISH/Gitalist-0.000003 from CPAN.
    gitpan-cpan-distribution: Gitalist
    gitpan-cpan-version:      0.000003
    gitpan-cpan-path:         BOBTFISH/Gitalist-0.000003.tar.gz
    gitpan-cpan-author:       BOBTFISH
    gitpan-cpan-maturity:     released

--- lib/Gitalist/Git/Repo.pm
+++ lib/Gitalist/Git/Repo.pm
@@ -1,38 +1,10 @@
 use MooseX::Declare;
 
-=head1 NAME
-
-Gitalist::Git::Repo - Model of a repository directory
-
-=head1 SYNOPSIS
-
-    my $repo = Gitalist::Git::Repo->new( repo_dir => $Dir );
-    my $project_list = $repo->projects;
-    my $first_project = @$project_list[0];
-    my $named_project = $repo->project('Gitalist');
-
-=head1 DESCRIPTION
-
-This class models a Gitalist Repo, which is a collection of
-Projects (git repositories).  It is used for creating Project
-objects to work with.
-
-=cut
-
-
 class Gitalist::Git::Repo {
     use MooseX::Types::Common::String qw/NonEmptySimpleStr/;
     use MooseX::Types::Path::Class qw/Dir/;
     use MooseX::Types::Moose qw/ArrayRef/;
     use aliased 'Gitalist::Git::Project';
-
-=head1 ATTRIBUTES
-
-=head2 repo_dir
-
-L<Path::Class::Dir> for the root of the Repo.
-
-=cut
 
     has repo_dir => (
         isa => Dir,
@@ -41,13 +13,6 @@ L<Path::Class::Dir> for the root of the Repo.
         coerce => 1,
     );
 
-=head2 projects
-
-An array of L<Gitalist::Git::Project> for each valid git repo
-found in repo_dir.
-
-=cut
-
     has projects => (
         is => 'ro',
         isa => ArrayRef['Gitalist::Git::Project'],
@@ -55,24 +20,17 @@ found in repo_dir.
         lazy_build => 1,
     );
 
-
-=head1 METHODS
-
-=head2 project
-
-Returns a L<Gitalist::Git::Project> for the specified project
-name.
-
-=cut
-
+    ## Public methods
     method project (NonEmptySimpleStr $project) {
         my $path = $self->repo_dir->subdir($project)->resolve;
+        $self->repo_dir->resolve; # FIXME - This needs to be called, or if repo_dir contains .., it'll explode below!
+                                  #         This is a Path::Class::Dir bug, right?
         die "Directory traversal prohibited" unless $self->repo_dir->contains($path);
         die "Not a valid Project" unless $self->_is_git_repo($path);
         return Project->new( $self->repo_dir->subdir($project) );
     }
 
-
+    ## Builders
     method _build_projects {
         my $base = $self->repo_dir;
         my $dh = $base->open || die "Could not open $base";
@@ -90,6 +48,7 @@ name.
         return [sort { $a->name cmp $b->name } @ret];
     }
 
+    ## Private methods
     # Determine whether a given directory is a git repo.
     method _is_git_repo ($dir) {
         return -f $dir->file('HEAD') || -f $dir->file('.git', 'HEAD');
@@ -98,9 +57,48 @@ name.
 
 __END__
 
+=head1 NAME
+
+Gitalist::Git::Repo - Model of a repository directory
+
+=head1 SYNOPSIS
+
+    my $repo = Gitalist::Git::Repo->new( repo_dir => $Dir );
+    my $project_list = $repo->projects;
+    my $first_project = @$project_list[0];
+    my $named_project = $repo->project('Gitalist');
+
+=head1 DESCRIPTION
+
+This class models a Gitalist Repo, which is a collection of
+Projects (git repositories).  It is used for creating Project
+objects to work with.
+
+
+=head1 ATTRIBUTES
+
+=head2 repo_dir (C<Path::Class::Dir>)
+
+The filesystem root of the Repo.
+
+=head2 projects
+
+An array of all Repos found in C<repo_dir>.
+
+
+
+=head1 METHODS
+
+=head2 project (Str $project)
+
+Returns a L<Gitalist::Git::Project> for the specified project
+name.
+
+
 =head1 SEE ALSO
 
 L<Gitalist::Git::Project>
+
 
 =head1 AUTHORS
 
