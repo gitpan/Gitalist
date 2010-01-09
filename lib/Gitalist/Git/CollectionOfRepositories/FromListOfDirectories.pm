dc13641a1399137228bfafcda04675bc680cb3df
Tomas Doran <bobtfish@bobtfish.net>
2010-01-09 15:48:31 +0000

    Import of BOBTFISH/Gitalist-0.000005 from CPAN.
    gitpan-cpan-distribution: Gitalist
    gitpan-cpan-version:      0.000005
    gitpan-cpan-path:         BOBTFISH/Gitalist-0.000005.tar.gz
    gitpan-cpan-author:       BOBTFISH
    gitpan-cpan-maturity:     released

--- lib/Gitalist/Git/CollectionOfRepositories/FromListOfDirectories.pm
+++ lib/Gitalist/Git/CollectionOfRepositories/FromListOfDirectories.pm
@@ -3,7 +3,9 @@ use MooseX::Declare;
 class Gitalist::Git::CollectionOfRepositories::FromListOfDirectories with Gitalist::Git::CollectionOfRepositories {
     use MooseX::Types::Common::String qw/NonEmptySimpleStr/;
     use MooseX::Types::Moose qw/ ArrayRef HashRef /;
+    use MooseX::Types::Path::Class qw/Dir/;
     use File::Basename qw/basename/;
+    use Path::Class qw/dir/;
     use Moose::Autobox;
 
     has repos => (
@@ -11,8 +13,8 @@ class Gitalist::Git::CollectionOfRepositories::FromListOfDirectories with Gitali
         is => 'ro',
         required => 1,
     );
-    has repos_by_name => (
-        isa => HashRef[NonEmptySimpleStr],
+    has _repos_by_name => (
+        isa => HashRef[Dir],
         is => 'ro',
         lazy_build => 1,
         traits => ['Hash'],
@@ -21,14 +23,52 @@ class Gitalist::Git::CollectionOfRepositories::FromListOfDirectories with Gitali
         },
     );
 
-    method _build_repos_by_name {
-        { map { basename($_) => $_ } $self->repos->flatten };
+    method _build__repos_by_name {
+        +{ map { basename($_) => dir($_) } $self->repos->flatten };
     }
 
     ## Builders
     method _build_repositories {
-        [ map { $self->get_repository($_) } $self->repos->flatten ];
+        [ map { $self->get_repository(basename($_)) } $self->repos->flatten ];
     }
 }                               # end class
 
 1;
+
+=head1 NAME
+
+Gitalist::Git::CollectionOfRepositories::FromListOfDirectories - Model of a collection of git repositories
+
+=head1 SYNOPSIS
+
+    my $collection = Gitalist::Git::CollectionOfRepositories::FromListOfDirectories->new( repos => [qw/
+        /path/to/repos1
+        /path/to/repos2
+    /] );
+    my $repository_list = $collection->repositories;
+    my $first_repository = $repository_list->[0];
+    my $named_repository = $repo->get_repository('Gitalist');
+
+=head1 DESCRIPTION
+
+This class provides an abstraction for a list of Repository directories.
+
+=head1 ATTRIBUTES
+
+=head2 repos (C<< ArrayRef[NonEmptySimpleStr] >>)
+
+A list of git repository directories
+
+=head1 SEE ALSO
+
+L<Gitalist::Git::CollectionOfRepositories>, L<Gitalist::Git::Repository>
+
+=head1 AUTHORS
+
+See L<Gitalist> for authors.
+
+=head1 LICENSE
+
+See L<Gitalist> for the license.
+
+=cut
